2b204f8cd78c48f6bf88046a9d159df6f2ddffc4
turbopro <dantepoe@gmail.com>
2019-10-21 18:58:15 -0400

    Complete FDA Assignemnt 2

---
+++ firstApp/FirstConsoleApp1/MainModule.vb
@@ -0,0 +1,84 @@
+﻿Module MainModule
+
+    ' Global variables that are available to the entire module
+    Dim tol As Double = 0.00000001      ' tolerance
+    Dim maxIters As Integer = 100       ' number of iterations
+    Dim root As Double                  ' variable to store root value
+
+
+    Sub Main()
+
+        ' Run test sub routines on three polynomials:
+
+        Console.WriteLine("Attempting to get roots for y = x^3:")
+        TestSecantOne()
+        Console.WriteLine("--------------------------------------" & vbCrLf)
+        Console.WriteLine("Click any key to continue" & vbCrLf)
+        Console.ReadKey()
+
+        Console.WriteLine("Attempting to get roots for y = x^2 + 1:")
+        TestSecantTwo()
+        Console.WriteLine("--------------------------------------" & vbCrLf)
+        Console.WriteLine("Click any key to continue" & vbCrLf)
+        Console.ReadKey()
+
+        Console.WriteLine("Attempting to get roots for y = -2x^3 + x^2 - x + 0.5:")
+        TestSecantThree()
+        Console.WriteLine("--------------------------------------" & vbCrLf)
+        Console.WriteLine("Click any key to quit" & vbCrLf)
+        Console.ReadKey()
+
+    End Sub
+
+    ' Test Subroutines to attempt to find roots for the following polynomials:
+
+    ' a) TestSecantOne():  y = x^3, initial guesses x0 = -1 , x1 = 1, tol = 0.00000001, maxIters =100
+    Public Sub TestSecantOne()
+        Dim vec0() As Double = {1, 0, 0, 0}     ' Set vector of polynomial coefficients
+        Dim x0 As Double = -1                   ' Set initial guess
+        Dim x1 As Double = 1                    ' Set initial guess
+
+        root = CubicSecant(vec0, x0, x1, tol, maxIters)     ' try to get root 
+
+        displayRoot(root)                       ' Display results
+
+    End Sub
+
+
+    ' b) TestSecantTwo():  y = x^2 + 1, initial guesses x0 = -0.5, x1 = 1, 
+    '    tol = 0.00000001, maxIters =100
+    Public Sub TestSecantTwo()
+        Dim vec0() As Double = {0, 1, 0, 1}
+        Dim x0 As Double = -0.5
+        Dim x1 As Double = 1
+
+        root = CubicSecant(vec0, x0, x1, tol, maxIters)
+
+        displayRoot(root)
+
+    End Sub
+
+    ' c) TestSecantThree():  y = -2x^3 + x^2 - x + 0.5, initial guesses x0 = -1, 
+    '    x1 = 1, tol = 0.00000001, maxIters =100
+    Public Sub TestSecantThree()
+        Dim vec0() As Double = {-2, 1, -1, 0.5}
+        Dim x0 As Double = -1
+        Dim x1 As Double = 1
+
+        root = CubicSecant(vec0, x0, x1, tol, maxIters)
+
+        displayRoot(root)
+
+    End Sub
+
+    ' displayRoot(.) to display root values
+    Public Sub displayRoot(ByVal root)
+        If Double.IsNaN(root) Then          ' Check if root is a NaN
+            Console.WriteLine("No root found.")
+            Console.WriteLine("No convergence after 100 iterations.")
+        Else
+            Console.WriteLine("A root was found at x = {0}", root)
+        End If
+    End Sub
+
+End Module
---
+++ firstApp/FirstConsoleApp1/My Project/Polyroot.vb
@@ -0,0 +1,71 @@
+﻿Module Polyroot
+    ' Polyroot Module Description:
+
+    ' Implements the Secant Method for determining the root of a cubic polynomial a x3 + b x2 + cx + d
+
+    ' Contains two functions:
+
+    ' Function Poly(.) that will return the value of an arbitrary cubic polynomial.
+    ' Poly(.) takes a vector, v, that contains the four constants in the polynomial, 
+    ' and a value x at which the function is to be evaluated.
+
+    ' Function CubicSecant(.) takes the same vector, v, of polynomial constants, 
+    ' initial guesses x0 And x1 for the Secant Method algorithm, 
+    ' a tolerance value tol For convergence, 
+    ' and an integer value maxIters that represents the maximum number Of iterations.
+    ' We implement the algorithm inside this function, but call Poly(.) on each iteration
+    ' to get the updated value of the cubic polynomial function.
+    ' Return the root found, or where there is no convergence, return "Double.NaN" 
+    ' "Double.NaN" is the VB.NET method For indicating "Not a Number"
+
+    ' CubicSecant(.) is called from the outside world so is declared 'Public'
+    ' Poly(.) is called only by the CubicSecant(.) function inside this module, and is declared 'Private'
+
+    '----------------------------------------------------------------------------------------------------
+
+    ' Function Definitions:
+
+    ' Function Poly(.)
+    Private Function Poly(ByVal v As Double(), ByVal x As Double) As Double
+        Dim result As Double = 0.0           ' accumulator
+        Dim y As Integer = v.Length - 1      ' exponent
+
+        For Each i As Integer In v
+            result += i * Math.Pow(x, y)     ' add  to result the coef multiplied by x^exponent
+            y -= 1                           ' decrement exponent
+        Next
+
+        Poly = result
+
+    End Function
+
+    ' Function CubicSecant(.)
+    ' Formula for Secant Method: 
+    ' Xn+1 = Xn – f(Xn) * ((Xn – Xn-1) / (f(Xn) – f(Xn-1)))
+    Public Function CubicSecant(ByVal v As Double(), ByVal x0 As Double,
+                                ByVal x1 As Double, ByVal tol As Integer,
+                                ByVal maxIters As Integer) As Double
+        Dim Xc As Double = x0        ' current value of X
+        Dim Xp As Double = x1        ' previous value of X
+        Dim Xu As Double             ' updated value of X
+
+        ' Loop up to maxIters times
+        ' On each iteration:
+        '   Update Xu, Xp, And Xc
+        '   Return Xu only if difference (absolute) between Xc and Xp is within tolerance
+
+        For i As Integer = 1 To maxIters
+            Xu = Xc - Poly(v, Xc) * ((Xc - Xp) / (Poly(v, Xc) - Poly(v, Xp)))
+            Xp = Xc
+            Xc = Xu
+
+            If Math.Abs(Xc - Xp) <= tol Then    ' root found
+                Return Xu
+            End If
+        Next
+
+        Return Double.NaN       ' no convegergence
+
+    End Function
+
+End Module
