30e6a8bc593b3d2bf56e64d4cfb9de44cc2ad853
vern <dantepoe@gmail.com>
2021-01-12 23:12:15 -0500

    Update Programming-Assignment_1.cpp, Jan 12, 23:12

--- CFRM520_00/Programming-Assignment_1/Programming-Assignment_1.cpp
+++ CFRM520_00/Programming-Assignment_1/Programming-Assignment_1.cpp
@@ -14,43 +14,41 @@ using std::cout;
 using std::endl;
 
 // Q2
-// function signature for trig_shift()
+// function declaration/signature for trig_shift()
 double trig_shift(double x, double y, double phi);
 
 // Q3
-// function signature for norm_cdf()
+// function declaration/signature for norm_cdf()
 double norm_cdf(double x);
 
 // Q4
-// function signature for sum_abs()
+// function declaration/signature for sum_abs()
 int sum_abs(int j, int k);
 
-// function signature for abs_sum()
+// function declaration/signature for abs_sum()
 int abs_sum(int j, int k);
+
 
 int main()
 {
-    //std::cout << "Hello World!\n";
-
+    /*
     cout << "trig_shift(0.1, 0.2, 0.3) = "
         << trig_shift(0.1, 0.2, 0.3)
-        << endl << endl;
-
-    cout << "norm_cdf(0.1) = "
+        << endl << endl
+        << "norm_cdf(0.1) = "
         << norm_cdf(0.1)
-        << endl << endl;
-
-    cout << "sum_abs(-5, 2) = " 
+        << endl << endl
+        << "sum_abs(-5, 2) = " 
         << sum_abs(-5, 2)
-        << endl << endl;
-
-    cout << "abs_sum(-5, 2) = "
+        << endl << endl
+        << "abs_sum(-5, 2) = "
         << abs_sum(-5, 2)
         << endl << endl;
-
+    */
 
     return 0;
 }
+
 
 // Q2
 // function definition for trig_shift()
@@ -59,7 +57,6 @@ double trig_shift(double x, double y, double phi)
     double result = std::sin(x + phi * y) + std::cos(x - phi * y);
     return result;
 }
-
 
 
 // Q3
@@ -73,32 +70,20 @@ double norm_cdf(double x)
 
     double result = (1 + std::erf(x / sqrt_two)) / 2;
 
-    //double cdf_0 = std::erf(x);
-
     return result;
 }
 
 
 // Q4
-// function signature for sum_abs()
+// function definition for sum_abs()
 int sum_abs(int j, int k)
 {
     return (std::abs(j) + std::abs(k));
 }
 
-// function signature for abs_sum()
+// function definition for abs_sum()
 int abs_sum(int j, int k)
 {
     return std::abs(j + k);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
